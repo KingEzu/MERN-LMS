5a4c8bb49f698b3156f5e77e7268f8b5ad04b436
Ezana <ezanamulugeta89@gmail.com>
2025-06-22 15:29:51 +0300

    main commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+//import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <Button className="bg-red-600">check</Button>
+    </>
+  )
+}
+
+export default App
